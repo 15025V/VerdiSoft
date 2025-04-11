44aa00e36cc3511413d7f21acbcae869ab76a463
15025V <vicogespinoza@gmail.com>
2025-04-10 21:57:15 -0600

    anali

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,35 @@
-
+import Script from "next/script";
 import Footer from "./components/footer";
 import NavBar from "./components/navBar";
 import { LanguageProvider } from "./context/languageContext";
 import "./globals.css";
 
-
-
 export default function RootLayout({ children }) {
   return (
     <LanguageProvider>
-      <html lang="en" translate="no" >
+      <html lang="en" translate="no">
+        <head />
         <body>
-         
-          {/*<NavBar />
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-CYMCBRPS98"
+            strategy="afterInteractive"
+          />
+          <Script id="gtag-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CYMCBRPS98');
+            `}
+          </Script>
+
+          {/* Tu contenido */}
+          {/* <NavBar /> */}
           {children}
-          <Footer />*/}
-              {children}
+          {/* <Footer /> */}
         </body>
       </html>
-
     </LanguageProvider>
   );
 }
