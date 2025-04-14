df5c0a12eeac159bc07298079592539051f43592
15025V <vicogespinoza@gmail.com>
2025-04-14 15:44:34 -0600

    site

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://flores-sons-farms.vercel.app',
+    generateRobotsTxt: true, // Opcional: Si también quieres generar un robots.txt automáticamente
+  };
+  
