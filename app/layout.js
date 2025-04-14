d217768b4dd297aaba70138059c09dae9cc8f469
15025V <vicogespinoza@gmail.com>
2025-04-14 14:49:12 -0600

    keys

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,34 @@ import NavBar from "./components/navBar";
 import { LanguageProvider } from "./context/languageContext";
 import "./globals.css";
 
+export const metadata = {
+  title: 'Flores Sons Farms - Productos Agrícolas Frescos y de Calidad',
+  description: 'Explora la riqueza de productos agrícolas frescos y de alta calidad de Flores Sons Farms. Cultivados con pasión y dedicación en el Valle de Palmarito.',
+  keywords: ['productos agrícolas', 'frescos', 'calidad', 'sostenibilidad', 'Flores Sons Farms'],
+  openGraph: {
+    title: 'Flores Sons Farms - Productos Agrícolas Frescos y de Calidad',
+    description: 'Explora la riqueza de productos agrícolas frescos y de alta calidad de Flores Sons Farms.',
+    url: 'https://flores-sons-farms.vercel.app/',
+    siteName: 'Flores Sons Farms',
+    images: [
+      {
+        url: 'https://flores-sons-farms.vercel.app/logo.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'es_MX',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Flores Sons Farms - Productos Agrícolas Frescos y de Calidad',
+    description: 'Explora la riqueza de productos agrícolas frescos y de alta calidad de Flores Sons Farms.',
+    images: ['https://flores-sons-farms.vercel.app/logo.png'],
+  },
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <LanguageProvider>
