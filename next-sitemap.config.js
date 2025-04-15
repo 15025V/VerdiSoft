80d22ff20a965f453348acf5d654ef80a8e86020
15025V <vicogespinoza@gmail.com>
2025-04-14 19:18:59 -0600

    site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,23 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://flores-sons-farms.vercel.app',
-    generateRobotsTxt: true, // Opcional: Si también quieres generar un robots.txt automáticamente
+    siteUrl: 'https://flores-sons-farms.vercel.app', // tu URL sin "/" al final
+    generateRobotsTxt: true,
+    outDir: './public',
+    transform: async (config, path) => {
+      return {
+        loc: path,
+        changefreq: 'monthly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      };
+    },
+    additionalPaths: async (config) => {
+      return [
+        { loc: '/', lastmod: new Date().toISOString() },
+        { loc: '/acerca-de-nosotros', lastmod: new Date().toISOString() },
+        { loc: '/catalogo-de-productos', lastmod: new Date().toISOString() },
+        { loc: '/contacto', lastmod: new Date().toISOString() },
+      ];
+    },
   };
   
